9b00f727614b1bd5acdb949307c77805b6125079
rbahena <rbahena@cjf.gob.mx>
2022-06-27 20:42:31 -0500

    Add oranization chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrganizationChartComponent } from './OrganizationChart/organization-chart/organization-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationChartComponent
   ],
   imports: [
     BrowserModule
