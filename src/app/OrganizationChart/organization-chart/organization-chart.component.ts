f9569d4f3e23ac8163f989ae1325f56d2c6da24e
rbahena <rbahena@cjf.gob.mx>
2022-06-27 23:32:22 -0500

    Delete useless images

--- src/app/OrganizationChart/organization-chart/organization-chart.component.ts
+++ src/app/OrganizationChart/organization-chart/organization-chart.component.ts
@@ -16,28 +16,28 @@ export class OrganizationChartComponent implements OnInit {
   tree: MyTreeNode = {
     name: 'Felines',
     description: 'Cute playful animals',
-    image: 'assets/chart-images/1.png',
+    image: 'assets/chart-images/9.png',
     onClick: () => alert('Death to dogs'),
     children: [
         {
             name: 'Big Cats',
-            image: 'assets/chart-images/3.jpg',
+            image: 'assets/chart-images/9.png',
             css: 'background-color: #F00000',
             children: [
                 {
                     name: 'Lion',
-                    image: 'assets/chart-images/6.png',
+                    image: 'assets/chart-images/9.png',
                     children: []
                 },
                 {
                     name: 'Tiger',
                     cssClass: 'yellow-on-hover',
-                    image: 'assets/chart-images/4.jpg',
+                    image: 'assets/chart-images/9.jpg',
                     children: []
                 },
                 {
                     name: 'Cheetah',
-                    image: 'assets/chart-images/8.png',
+                    image: 'assets/chart-images/9.png',
                     children: []
                 }
             ]
@@ -49,48 +49,48 @@ export class OrganizationChartComponent implements OnInit {
             children: [
                 {
                     name: 'House Cat',
-                    image: 'assets/chart-images/7.png',
+                    image: 'assets/chart-images/9.png',
                     children: []
                 },
                 {
                     name: 'Street Cat',
-                    image: 'assets/chart-images/8.png',
+                    image: 'assets/chart-images/9.png',
                     children: [
                         {
                             name: 'Dumb Cat',
-                            image: 'assets/chart-images/13.png',
+                            image: 'assets/chart-images/9.png',
                             children: [
                                 {
                                     name: 'Sorry For Bad Example',
-                                    image: 'assets/chart-images/6.png',
+                                    image: 'assets/chart-images/9.png',
                                     children: []
                                 }
                             ],
                         },
                         {
                             name: 'Good Cat',
-                            image: 'assets/chart-images/8.png',
+                            image: 'assets/chart-images/9.png',
                             children: [
                                 {
                                     name: 'Binary Search Tree',
-                                    image: 'assets/chart-images/10.png',
+                                    image: 'assets/chart-images/9.png',
                                     children: [
                                         {
                                             name: '7',
-                                            image: 'assets/chart-images/11.png',
+                                            image: 'assets/chart-images/9.png',
                                             children: [
                                                 {
                                                     name: '3',
-                                                    image: 'assets/chart-images/6.png',
+                                                    image: 'assets/chart-images/9.png',
                                                     children: [
                                                         {
                                                             name: '2',
-                                                            image: 'assets/chart-images/7.png',
+                                                            image: 'assets/chart-images/9.png',
                                                             children: []
                                                         },
                                                         {
                                                             name: '5',
-                                                            image: 'assets/chart-images/12.png',
+                                                            image: 'assets/chart-images/9.png',
                                                             children: []
                                                         }
                                                     ]
@@ -98,17 +98,17 @@ export class OrganizationChartComponent implements OnInit {
                                                 {
                                                     name: '13',
                                                     description: 'An odd yet funny number.',
-                                                    image: 'assets/chart-images/14.png',
+                                                    image: 'assets/chart-images/9.png',
                                                     children: [
                                                         {
                                                             name: '11',
                                                             description: 'All nodes to the right are greater',
-                                                            image: 'assets/chart-images/15.png',
+                                                            image: 'assets/chart-images/9.png',
                                                             children: []
                                                         },
                                                         {
                                                             name: '17',
-                                                            image: 'assets/chart-images/13.png',
+                                                            image: 'assets/chart-images/9.png',
                                                             description: 'This number is less that 17.00000001',
                                                             children: []
                                                         }
@@ -126,7 +126,7 @@ export class OrganizationChartComponent implements OnInit {
         },
         {
             name: 'Fake Cats',
-            image: 'assets/chart-images/5.png',
+            image: 'assets/chart-images/9.png',
             children: [],
             onClick: () => console.log('Google chrome stole some RAM')
         }
