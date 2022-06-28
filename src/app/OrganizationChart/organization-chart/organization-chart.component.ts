00e140380400af10f24b06a4c9891716b7f032cd
rbahena <rbahena@cjf.gob.mx>
2022-06-27 23:25:32 -0500

    Update project images

--- src/app/OrganizationChart/organization-chart/organization-chart.component.ts
+++ src/app/OrganizationChart/organization-chart/organization-chart.component.ts
@@ -16,121 +16,121 @@ export class OrganizationChartComponent implements OnInit {
   tree: MyTreeNode = {
     name: 'Felines',
     description: 'Cute playful animals',
-    image: 'assets/images/ironman.png',
+    image: 'assets/chart-images/1.png',
     onClick: () => alert('Death to dogs'),
     children: [
-      {
-        name: 'Big Cats',
-        image: 'assets/images/ironman.png',
-        css: 'background-color: #F00000',
-        children: [
-          {
-            name: 'Lion',
-            image: 'assets/images/ironman.png',
-            children: []
-          },
-          {
-            name: 'Tiger',
-            cssClass: 'yellow-on-hover',
-            image: 'assets/images/ironman.png',
-            children: []
-          },
-          {
-            name: 'Cheetah',
-            image: 'assets/images/ironman.png',
-            children: []
-          }
-        ]
-      },
-      {
-        name: 'Small Cats',
-        description: 'Cute, but can also be crude. Like when they defecate on your lap, that would be a good example of crudeness on their part',
-        image: 'assets/images/ironman.png',
-        children: [
-          {
-            name: 'House Cat',
-            image: 'assets/images/ironman.png',
-            children: []
-          },
-          {
-            name: 'Street Cat',
-            image: 'assets/images/ironman.png',
+        {
+            name: 'Big Cats',
+            image: 'assets/chart-images/3.jpg',
+            css: 'background-color: #F00000',
             children: [
-              {
-                name: 'Dumb Cat',
-                image: 'assets/images/ironman.png',
-                children: [
-                  {
-                    name: 'Sorry For Bad Example',
-                    image: 'assets/images/ironman.png',
+                {
+                    name: 'Lion',
+                    image: 'assets/chart-images/6.png',
                     children: []
-                  }
-                ],
-              },
-              {
-                name: 'Good Cat',
-                image: 'assets/images/ironman.png',
-                children: [
-                  {
-                    name: 'Binary Search Tree',
-                    image: 'assets/images/ironman.png',
-                    children: [
-                      {
-                        name: '7',
-                        image: 'assets/images/ironman.png',
-                        children: [
-                          {
-                            name: '3',
-                            image: 'assets/images/ironman.png',
-                            children: [
-                              {
-                                name: '2',
-                                image: 'assets/images/ironman.png',
-                                children: []
-                              },
-                              {
-                                name: '5',
-                                image: 'assets/images/ironman.png',
-                                children: []
-                              }
-                            ]
-                          },
-                          {
-                            name: '13',
-                            description: 'An odd yet funny number.',
-                            image: 'assets/images/ironman.png',
-                            children: [
-                              {
-                                name: '11',
-                                description: 'All nodes to the right are greater',
-                                image: 'assets/images/ironman.png',
-                                children: []
-                              },
-                              {
-                                name: '17',
-                                image: 'assets/images/ironman.png',
-                                description: 'This number is less that 17.00000001',
-                                children: []
-                              }
-                            ]
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ]
-              }
+                },
+                {
+                    name: 'Tiger',
+                    cssClass: 'yellow-on-hover',
+                    image: 'assets/chart-images/4.jpg',
+                    children: []
+                },
+                {
+                    name: 'Cheetah',
+                    image: 'assets/chart-images/8.png',
+                    children: []
+                }
             ]
-          }
-        ]
-      },
-      {
-        name: 'Fake Cats',
-        image: 'assets/images/ironman.png',
-        children: [],
-        onClick: () => console.log('Google chrome stole some RAM')
-      }
+        },
+        {
+            name: 'Small Cats',
+            description: 'Cute, but can also be crude. Like when they defecate on your lap, that would be a good example of crudeness on their part',
+            image: 'assets/chart-images/9.png',
+            children: [
+                {
+                    name: 'House Cat',
+                    image: 'assets/chart-images/7.png',
+                    children: []
+                },
+                {
+                    name: 'Street Cat',
+                    image: 'assets/chart-images/8.png',
+                    children: [
+                        {
+                            name: 'Dumb Cat',
+                            image: 'assets/chart-images/13.png',
+                            children: [
+                                {
+                                    name: 'Sorry For Bad Example',
+                                    image: 'assets/chart-images/6.png',
+                                    children: []
+                                }
+                            ],
+                        },
+                        {
+                            name: 'Good Cat',
+                            image: 'assets/chart-images/8.png',
+                            children: [
+                                {
+                                    name: 'Binary Search Tree',
+                                    image: 'assets/chart-images/10.png',
+                                    children: [
+                                        {
+                                            name: '7',
+                                            image: 'assets/chart-images/11.png',
+                                            children: [
+                                                {
+                                                    name: '3',
+                                                    image: 'assets/chart-images/6.png',
+                                                    children: [
+                                                        {
+                                                            name: '2',
+                                                            image: 'assets/chart-images/7.png',
+                                                            children: []
+                                                        },
+                                                        {
+                                                            name: '5',
+                                                            image: 'assets/chart-images/12.png',
+                                                            children: []
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    name: '13',
+                                                    description: 'An odd yet funny number.',
+                                                    image: 'assets/chart-images/14.png',
+                                                    children: [
+                                                        {
+                                                            name: '11',
+                                                            description: 'All nodes to the right are greater',
+                                                            image: 'assets/chart-images/15.png',
+                                                            children: []
+                                                        },
+                                                        {
+                                                            name: '17',
+                                                            image: 'assets/chart-images/13.png',
+                                                            description: 'This number is less that 17.00000001',
+                                                            children: []
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Fake Cats',
+            image: 'assets/chart-images/5.png',
+            children: [],
+            onClick: () => console.log('Google chrome stole some RAM')
+        }
     ]
-  };
+};
 
 }
